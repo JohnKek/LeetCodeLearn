45ad23c9dff9f052467e299cf7091c5b98d4adf6
igta0222 <igor.tatarinov@netcracker.com>
2024-04-20 22:57:21 +0300

    412

---
+++ 412.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fizzBuzz(n int) []string {
+	var answer []string
+	for i := 1; i <= n; i++ {
+		answer = append(answer, findFizzBuzz(i))
+	}
+	return answer
+}
+
+func findFizzBuzz(i int) string {
+	switch {
+	case i%15 == 0:
+		return "FizzBuzz"
+	case i%5 == 0:
+		return "Buzz"
+	case i%3 == 0:
+		return "Fizz"
+	default:
+		return strconv.Itoa(i)
+	}
+}
+
+func init() {
+	nums := 3
+	fmt.Println(fizzBuzz(nums))
+}
